485de2824bfc92b9ea48f9f49d31edd2bc5b0feb
datapiotr <p.data@interia.pl>
2022-02-25 12:10:33 +0100

    style corrections

--- game.js
+++ game.js
@@ -45,26 +45,26 @@ function publishResult(player, ai, result) {
 
     if (result === 'Win!') {
         document.querySelector('p.wins span').textContent = ++gameSummary.wins
-        document.querySelector('[data-summary="who-win"]').textContent = "Ty wygrałeś"
+        document.querySelector('[data-summary="who-win"]').textContent = "You win"
         document.querySelector('[data-summary="who-win"]').style.color = 'green'
     } else if (result === 'Loss!') {
         document.querySelector('p.losses span').textContent = ++gameSummary.losses
-        document.querySelector('[data-summary="who-win"]').textContent = "Komputer wygrał"
+        document.querySelector('[data-summary="who-win"]').textContent = "Computer win"
         document.querySelector('[data-summary="who-win"]').style.color = 'red'
     } else {
         document.querySelector('p.draws span').textContent = ++gameSummary.draws
-        document.querySelector('[data-summary="who-win"]').textContent = "Remis"
+        document.querySelector('[data-summary="who-win"]').textContent = "Draw"
         document.querySelector('[data-summary="who-win"]').style.color = 'black'
     }
 }
 
 function endGame() {
-    document.querySelector(`[data-option="${game.playerHand}"]`).style.boxShadow = ''
+    document.querySelector(`[data-option="${game.playerHand}"]`).style.transform = 'scale(1)'
     game.playerHand = ''
 }
 
 function startGame() {
-    if (!game.playerHand) return alert('wybierz dłoń!')
+    if (!game.playerHand) return alert('check a hand!')
     game.aiHand = aiChoice()
     const gameResult = checkResult(game.playerHand, game.aiHand)
     console.log(gameResult)
